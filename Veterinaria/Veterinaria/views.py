5176633a4cacddfcbb9513cee41da051cad0ba74
GerBelucci <gbeluccigaribotto@frba.utn.edu.ar>
2022-11-23 17:48:50 -0300

    cambios

--- Veterinaria/Veterinaria/views.py
+++ Veterinaria/Veterinaria/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.template import Template, Context, loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from CRUD import Database
 
 db = Database()
-def login(request, error = ""):
-    return render(request, "login.html", {"error" : error})
+def login(request):
+    return render(request, "login.html")
 
-def register(request, error = ""):
-    return render(request, "register.html", {"error" : error})
+def register(request):
+    return render(request, "register.html")
 
 def post_register(request):
     email_usr = request.POST["email"]
     if db.userExists(email_usr): # ver como hacer para checkear existencia de usuario
-        return register(request, "El mail ya esta registrado")
+        messages.error("El mail ya esta registrado")
+        return redirect('register')
     else:
         nombre_usr = request.POST["nombre"]
         apellido_usr = request.POST["apellido"]
@@ -31,14 +32,16 @@ def post_usuario(request):
     if db.login(email_usr,contrasenia_usuario):
         return inicio(request)
     else:
-        return login(request, "Los datos ingresados son incorrectos o no se corresponden con un usuario registrado")
+        messages.error("Los datos ingresados son incorrectos o no se corresponden con un usuario registrado")
+        return redirect('login')
 
 def inicio(request):
     usuario = db.user(request.POST["email"]) #crear metodo user en CRUD 
     return render(request, "index.html", {"nombre" : usuario.nombre, "apellido" : usuario.apellido}) 
 
 def agendar_consulta(request):
-    return render(request, "ingreso_consulta.html")        
+    usuario = db.user(request.POST["email"]) #cambiar por la implementacion en CRUD
+    return render(request, "ingreso_consulta.html", {"nombre" : usuario.nombre, "apellido" : usuario.apellido})        
 
 def post_consulta(request):
     usuario = request.POST["usuario"]
